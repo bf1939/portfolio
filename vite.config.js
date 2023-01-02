59a1e177da2d391ca88acb06ea231f87292339c3
bf910 <121231212+bf910@users.noreply.github.com>
2023-01-03 00:12:01 +0900

    github actionsでデプロイできるようにした (#7)
    * base追加
    
    * deploy.yaml作成
    
    * permissionsにcontents追加
    
    * 環境変数BASE_URL追加
    
    * permissionsを移動
    
    * ダブルクォーテーション削除
    
    * branchesをmainに変更
    
    * groupをdeployに変更

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import path from "path";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 
+const { BASE_URL } = process.env;
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE_URL || "/",
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
